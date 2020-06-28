b902c8019800bfb28629bc1c373d61ab5fb9f204
grifon613 <linmihmas@gmail.com>
2020-06-28 18:07:41 +0300

    add h9

---
+++ h9/js/script.js
@@ -0,0 +1,95 @@
+let memory = [];
+
+function calculate(data){
+	let operator = ["+", "-", "*", "/", "%"];
+	for(let i = 0; i < operator.length; i++){
+		for(let y = 0; y < data.length; y++){
+			if(operator[i] === data[y]){
+				switch(data[y]){
+					case "+": add(data);
+					break;
+					case "-": sub(data);
+					break;
+					case "*": mul(data);
+					break;
+					case "/": div(data);
+					break;
+					case "%": abs(data);
+					break;					
+				}
+			}
+		}
+	}
+}
+
+function add(data){
+	//alert(data);
+	let result = 0;
+	let number = data.split("+");
+	for(let i = 0; i < number.length; i++){
+		result += Number(number[i]);
+	}
+	document.querySelector("input").value = result;
+	memory.push('{operation: \"' + data + '\"', 'result: \"' + result + '\"},');
+	showHistory(memory);
+}
+
+function sub(data){
+	//alert(data);
+	let result = 0;
+	let number = data.split("-");
+	result = number[0];
+	for(let i = 1; i < number.length; i++){
+		result -= number[i];
+	}
+	document.querySelector("input").value = result;
+	memory.push('{operation: \"' + data + '\"', 'result: \"' + result + '\"},');
+	showHistory(memory);	
+}
+
+function mul(data){
+	//alert(data);
+	let result = 0;
+	let number = data.split("*");
+	result = number[0];
+	for(let i = 1; i < number.length; i++){
+		result *= number[i];
+	}
+	document.querySelector("input").value = result;
+	memory.push('{operation: \"' + data + '\"', 'result: \"' + result + '\"},');
+	showHistory(memory);	
+}
+
+function div(data){
+	//alert(data);
+	let result = 0;
+	let number = data.split("/");
+	result = number[0];
+	for(let i = 1; i < number.length; i++){
+		result /= number[i];
+	}
+	document.querySelector("input").value = result;
+	memory.push('{operation: \"' + data + '\"', 'result: \"' + result + '\"},');
+	showHistory(memory);	
+}
+
+function abs(data){
+	//alert(data);
+	let result = 0;
+	let number = data.split("%");
+	result = number[0];
+	for(let i = 1; i < number.length; i++){
+		result %= number[i];
+	}
+	document.querySelector("input").value = result;
+	memory.push('{operation: \"' + data + '\"', 'result: \"' + result + '\"},');
+	showHistory(memory);	
+}		
+
+function showHistory(memory){
+	let item = "";
+	for(let i = 0; i < memory.length; i++){
+		item += memory[i];
+	}
+	document.querySelector(".history").innerHTML = item;
+}
